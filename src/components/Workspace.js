f9ece33117bb40ddad77fdafc9930068cce2270e
Nahian Reza <nahian.reza@stonybrook.edu>
2021-10-01 15:12:27 -0400

    asda

--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -2,6 +2,7 @@ import React from "react";
 
 export default class Workspace extends React.Component {
     render() {
+        const{currentList} =this.props
         return (
             <div id="top5-workspace">
                 <div id="workspace-edit">
@@ -11,6 +12,11 @@ export default class Workspace extends React.Component {
                         <div className="item-number">3.</div>
                         <div className="item-number">4.</div>
                         <div className="item-number">5.</div>
+                    </div>
+                    <div id="edit-items">
+                        {currentList && currentList.items.map(function(name, index){
+                            return <div className="item-number" key={ index }> {name}</div>;
+                        })}
                     </div>
                 </div>
             </div>
